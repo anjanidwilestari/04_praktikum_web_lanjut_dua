4130d7ef077e9e20aca381edd03a8a4857777758
Ayi <ayianjani774@gmail.com>
2022-03-09 18:04:16 +0700

    edit new

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,42 @@
 @extends('layouts.app')
 
+@section('tittle')
+    {{ $title }}
+@endsection
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+    <div class="page-h promotion">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    <h3> DASHBOARD </h3>
                 </div>
             </div>
         </div>
     </div>
+    <div class="video-featured">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2">
+                    <div class="video-holder">
+                        <iframe width="560" height="315" src="https://www.youtube.com/embed/Abt1Uq2s848" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="fourth-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    <h2>SELAMAT DATANG, EDUCA</h2>
+                    <p>Yeay, Kamu baru saja logged in!</p>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@section('footer')
+    <p>By Kelompok 2 Anjani - Bella</p>
 </div>
 @endsection
--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -14,9 +14,27 @@
             </div>
         </div>
     </div>
-
-    <h1 class="mb-5">{{ news["title"] }}</h1>
-    <p>{{ $post["body"] }}</p>
+    <div class="video-featured">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2">
+                    <div class="video-holder">
+                        <iframe width="560" height="315" src="https://www.youtube.com/embed/_5A7-IkfpT0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="fourth-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    <h2>TELUSURI BERITAMU HARI INI!</h2>
+                    <p>Telusuri berita faktual dan up to date hanya di Educastudio Modify</p>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
 
 @section('footer')
