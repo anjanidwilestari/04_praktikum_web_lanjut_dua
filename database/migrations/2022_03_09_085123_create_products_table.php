c7ed1edf9b54e9eddd53b985de4b10e235955610
Ayi <ayianjani774@gmail.com>
2022-03-09 18:03:49 +0700

    mengisi product table

--- database/migrations/2022_03_09_085123_create_products_table.php
+++ database/migrations/2022_03_09_085123_create_products_table.php
@@ -15,6 +15,11 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('nama')->index();
+            $table->string('gambar')->index();
+            $table->string('jenis')->index();
+            $table->text('konten');
+            $table->boolean('status')->default(false);
             $table->timestamps();
         });
     }
